8f22e20a2e396d3ba20613e666f31ac9fb3efbcc
alaekwebartholomew <alaekwebartholomew@gmail.com>
2023-04-09 17:25:44 +0100

    slide show

--- typing_game.py
+++ typing_game.py
@@ -1,5 +1,20 @@
 from tkinter import *
 
+slideTexts = ""
+count = 0
+
+
+def welcomeTextSlideShow():
+    global slideTexts, count
+    string = "WELCOME!. IMPROVE YOUR TYPING SPEED"
+    slideTexts += string[count]
+    movingText.config(text=slideTexts)
+    count += 1
+    if count >= len(string):
+        count = 0
+        slideTexts = ""
+    movingText.after(300, welcomeTextSlideShow)
+
 
 root = Tk()
 root.title("Typing Game -- created by Barth")
@@ -11,7 +26,8 @@ root.config(background="DarkSeaGreen4")
 backgroundImg = PhotoImage(file='icons/clock.png')
 backgroundLabel = Label(root, image=backgroundImg, background="DarkSeaGreen4")
 backgroundLabel.place(x=70, y=100)
-movingText = Label(root, text="WELCOME!, IMPROVE YOUR TYPING SPEED",
+movingText = Label(root, text="WELCOME!. IMPROVE YOUR TYPING SPEED",
                    background="DarkSeaGreen4", font=("Courier", 10, "bold"), width=48)
 movingText.place(x=0, y=10)
+welcomeTextSlideShow()
 root.mainloop()
